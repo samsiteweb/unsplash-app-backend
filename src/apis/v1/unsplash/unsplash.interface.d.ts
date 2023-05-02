32e660f480ae70d8ff04aea62ae9ff60566821e9
samuel <samthedonz@gmail.com>
2023-05-02 06:14:23 +0100

    added repository for unsplash

---
+++ src/apis/v1/unsplash/unsplash.interface.d.ts
@@ -0,0 +1,9 @@
+
+
+export interface ImageStore {
+    id: string;
+    image_url: string;
+    label: string;
+    created_at: Date;
+    updated_at: Date;
+}
---
+++ src/apis/v1/unsplash/unsplash.repository.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+import { ImageStore } from './unsplash.interface';
+
+const prisma = new PrismaClient();
+
+export const addImageRepo = async (data: Partial<ImageStore>): Promise<ImageStore> => {
+  const newImage = await prisma.imageStore.create({
+    data: {
+      image_url: data.image_url,
+      label: data.label
+    }
+  });
+  return newImage;
+};
+export const fetchImagesRepo = async (filters: any): Promise<ImageStore[]> => {
+  const images = await prisma.imageStore.findMany({
+    where: { ...filters }
+  });
+  return images;
+};
+export const deleteImageRepo = async (filters: Partial<ImageStore>): Promise<ImageStore> => {
+  const image = await prisma.imageStore.delete({
+    where: { ...filters }
+  });
+  return image;
+};
