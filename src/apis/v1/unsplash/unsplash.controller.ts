97c27c5d7f2d8cc2a4114616b7872bf0c448b374
samuel <samthedonz@gmail.com>
2023-05-02 06:29:22 +0100

    added unsplash controller

--- src/apis/v1/unsplash/unsplash.controller.ts
+++ src/apis/v1/unsplash/unsplash.controller.ts
@@ -1,29 +1,32 @@
 import { respond } from '@src/utilities';
 import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { ImageStore } from './unsplash.interface';
+import { addImageService, deleteImageService, fetchImageService } from './unsplash.service';
 
 const unsplashController = {
   addImage: (): RequestHandler => async (req, res, next) => {
     try {
-      const role = req.body.role;
-      
-      // respond(res, createdRole, StatusCodes.OK);
+      const image: Partial<ImageStore> = req.body;
+      const storedImage = await addImageService(image)
+      respond(res, storedImage, StatusCodes.OK);
     } catch (error) {
       next(error);
     }
   },
   fetchImages: (): RequestHandler => async (req, res, next) => {
     try {
-      const role = req.body.role;
-      
-      // respond(res, createdRole, StatusCodes.OK);
+      const storedImages = await fetchImageService()
+      respond(res, storedImages, StatusCodes.OK);
     } catch (error) {
       next(error);
     }
   },
   deleteImages: (): RequestHandler => async (req, res, next) => {
     try {
-  
+      const {imageId} = req.query.id as Record<string, string>;
+      
+      const deleteImage = await deleteImageService(imageId)
     } catch (error) {
       next(error);
     }
