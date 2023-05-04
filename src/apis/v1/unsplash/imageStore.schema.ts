57cd5fa2dcb294a62ca3685158c31f8b2d796d62
samuel <samthedonz@gmail.com>
2023-05-04 08:57:05 +0100

    lastest commit

--- src/apis/v1/unsplash/imageStore.schema.ts
+++ src/apis/v1/unsplash/imageStore.schema.ts
@@ -1,23 +1,12 @@
 
 import { celebrate, Joi, Segments } from 'celebrate';
 
-export const fetchImageSchema = celebrate(
-  {
-    [Segments.QUERY]: Joi.object().keys({
-      page: Joi.number(),
-      perPage: Joi.number()
-    })
-  },
-  {
-    abortEarly: false
-  }
-);
 
 export const addImageSchema = celebrate(
   {
     [Segments.BODY]: Joi.object().keys({
-      image_url: Joi.number(),
-      label: Joi.number(),
+      image_url: Joi.string().required().trim(),
+      label: Joi.string().required().trim()
     })
   },
   {
@@ -30,9 +19,7 @@ export const addImageSchema = celebrate(
 export const searchImageSchema = celebrate(
   {
     [Segments.QUERY]: Joi.object().keys({
-      queryString: Joi.string().required().trim().allow(''),
-      page: Joi.string().required().trim().allow(''),
-      perPage: Joi.string().required().trim().allow('')
+      queryString: Joi.string().required().trim().allow('')
     })
   },
   {
